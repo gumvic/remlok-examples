5e6ee66735627493d56d015a6872ec466ad11660
Viktor Osychniuk <osyuchnyuk@interesnee.ru>
2016-05-18 16:59:36 +0300

    remove devsrc

--- devsrc/counter/dev.cljs
+++
@@ -1,6 +0,0 @@
-(ns counter.dev
-  (:require [counter.core :as example]
-            [figwheel.client :as fw]))
-
-(fw/start {:on-jsload example/run
-           :websocket-url "ws://localhost:3449/figwheel-ws"})
--- devsrc/wiki/dev.cljs
+++
@@ -1,6 +0,0 @@
-(ns wiki.dev
-  (:require [wiki.core :as example]
-            [figwheel.client :as fw]))
-
-(fw/start {:on-jsload example/run
-           :websocket-url "ws://localhost:3449/figwheel-ws"})
